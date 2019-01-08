956456ad4e6de021ff86f9a15a5993b5ade4bda7
Tony_Huang <tong@Tony-Huangs-MacBook-Pro.local>
2019-01-08 15:38:57 -0500

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,16 @@
 def count_elements(array)
   # code goes here
+  new_hash={}
+  
+  array.uniq.each do |i|
+  count=1
+    array.each do |j|
+      if j==i
+        new_hash[i]=count
+        count+=1
+      end
+    end
+  end
+  return new_hash
 end
  
